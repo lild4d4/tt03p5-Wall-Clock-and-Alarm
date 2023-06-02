f66ce16df9d6d1028bb54868ddd292a6394dbf91
Daniel Arevalos <64666124+lild4d4@users.noreply.github.com>
2023-06-02 11:07:04 -0600

    Update tt_um_Reloj_top.v

--- src/tt_um_Reloj_top.v
+++ src/tt_um_Reloj_top.v
@@ -40,7 +40,7 @@ module Reloj_top(
 	
     reg [6:0] segments;
     reg [7:0] anodes;
-	assign uio_out [6:0] = {1'b1, segments};
+    assign uio_out [6:0] = segments;
     assign uo_out 			= anodes; 
 	
     assign uio_en = 8'b11111111;
