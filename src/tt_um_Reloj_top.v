98905d7a9e27e6aa596f92e8d35be37fa51fbd9a
Daniel Arevalos <64666124+lild4d4@users.noreply.github.com>
2023-06-02 12:24:13 -0600

    Update tt_um_Reloj_top.v

--- src/tt_um_Reloj_top.v
+++ src/tt_um_Reloj_top.v
@@ -24,7 +24,7 @@ module tt_um_Reloj_top(
     input wire [7:0] ui_in,
     input wire [7:0] uio_in,
     output wire [7:0] uo_out,
-    input wire [7:0] uio_out,
+    output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
     input wire ena, clk, rst_n
     );
