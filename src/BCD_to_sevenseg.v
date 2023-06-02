6565f433e92220baeeb18b5edf0cbbf2ded11cc3
Daniel Arevalos <64666124+lild4d4@users.noreply.github.com>
2023-06-02 11:11:09 -0600

    Update BCD_to_sevenseg.v

--- src/BCD_to_sevenseg.v
+++ src/BCD_to_sevenseg.v
@@ -27,23 +27,23 @@ module BCD_to_sevenseg(
     
     always @(*) begin       //notar que los segmentos funcionan con logica inversa
         case(BCD)
-            4'd0: sevenseg = ~7'b1111110; // abcdefg
-            4'd1: sevenseg = ~7'b0110000;
-            4'd2: sevenseg = ~7'b1101101;
-            4'd3: sevenseg = ~7'b1111001;
-            4'd4: sevenseg = ~7'b0110011;
-            4'd5: sevenseg = ~7'b1011011;
-            4'd6: sevenseg = ~7'b1011111;
-            4'd7: sevenseg = ~7'b1110000;
-            4'd8: sevenseg = ~7'b1111111;
-            4'd9: sevenseg = ~7'b1111011;
-            4'd10: sevenseg =~7'b1110111;
-            4'd11: sevenseg =~7'b0011111;
-            4'd12: sevenseg =~7'b1001110;
-            4'd13: sevenseg =~7'b0111101;
-            4'd14: sevenseg =~7'b1001111;
-            4'd15: sevenseg =~7'b1000111;
-            default: sevenseg = ~7'b0000000;
+            4'd0: sevenseg = 7'b0000001; // abcdefg
+            4'd1: sevenseg = 7'b0001111;
+            4'd2: sevenseg = 7'b0010010;
+            4'd3: sevenseg = 7'b0000110;
+            4'd4: sevenseg = 7'b1001100;
+            4'd5: sevenseg = 7'b0100100;
+            4'd6: sevenseg = 7'b0100000;
+            4'd7: sevenseg = 7'b0001111;
+            4'd8: sevenseg = 7'b0000000;
+            4'd9: sevenseg = 7'b0000100;
+            4'd10: sevenseg =7'b0001000;
+            4'd11: sevenseg =7'b1100000;
+            4'd12: sevenseg =7'b0110001;
+            4'd13: sevenseg =7'b1000010;
+            4'd14: sevenseg =7'b0110000;
+            4'd15: sevenseg =7'b0111000;
+            default: sevenseg = 7'b1111111;
         endcase
     end
 endmodule
